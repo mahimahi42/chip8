3b68c79ac7ed9e27dc971d197ebd0fdc549512db
Bryce Davis <me@bryceadavis.com>
2019-07-31 19:35:00 -0400

    Add basic CPU skeleton

---
+++ src/chip8cpu.rs
@@ -0,0 +1,62 @@
+use std::fs::File;
+use std::io::{Read, Result};
+
+const RAM: usize = 4096;
+const PROG_START: usize = 0x200;
+
+pub struct Cpu {
+    reg_v: [u8; 16],
+    reg_i: u16,
+    reg_d: u8,
+    reg_s: u8,
+    pub pc: usize,
+    sp: usize,
+    stack: [u16; 16],
+    ram: [u8; RAM],
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            reg_v: [0; 16],
+            reg_i: 0,
+            reg_d: 0,
+            reg_s: 0,
+            pc: PROG_START,
+            sp: 0,
+            stack: [0; 16],
+            ram: [0; RAM]
+        }
+    }
+
+    pub fn load_rom(&mut self, path: &str) {
+        if let Ok(rom) = self.read_rom(path) {
+            let rom_data: &[u8] = &rom;
+            for (i, &data) in rom_data.iter().enumerate() {
+                let mem_addr = PROG_START + i;
+                if mem_addr < 4096 {
+                    self.ram[mem_addr] = data;
+                }
+            }
+        } else {
+            println!("COULD NOT LOAD ROM {}", path);
+        }
+    }
+
+    fn read_rom(&self, path: &str) -> Result<Vec<u8>> {
+        let mut file = File::open(path)?;
+
+        let mut data = Vec::new();
+        file.read_to_end(&mut data)?;
+
+        return Ok(data);
+    }
+
+    pub fn opcode(&self) -> u16 {
+        (self.ram[self.pc] as u16) << 8 | (self.ram[self.pc + 1] as u16)
+    }
+
+    fn unimplemented_opcode(opcode: u16) -> String {
+        format!("Unimplemented opcode {:#06X}", opcode)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{Arg, App};
+
+mod chip8cpu;
+use chip8cpu::Cpu;
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::new("CHIP-8 Emulator")
+                    .version("1.0")
+                    .author("Bryce Davis <me@bryceadavis.com>")
+                    .about("CHIP-8 Emulator written in Rust")
+                        .arg(Arg::with_name("input_file")
+                            .help("Input ROM file")
+                            .required(true)
+                            .index(1))
+                        .get_matches();
+
+    let input_file = args.value_of("input_file").unwrap();
+
+    let mut proc: Cpu = Cpu::new();
+
+    proc.load_rom(input_file);
+
+    println!("{:#06X} {:#06X}", proc.pc, proc.opcode());
 }
